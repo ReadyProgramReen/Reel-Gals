3945e8a606264ea3b5b74577491a4dcc9a297e98
Maureen E <maureenekeugo1@gmail.com>
2024-07-01 14:39:01 -0400

    added and styled the list of videos and added some functionalities

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let menuIcon = document.querySelector('.menu-icon')
 let sidebar = document.querySelector('.sidebar') 
+let conatiner = document.querySelector('.container') 
+
+
+
 
 
 menuIcon.onclick = ()=>{
     sidebar.classList.toggle('small-sidebar')
+    conatiner.classList.toggle('large-container')
 }
