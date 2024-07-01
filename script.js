f954e4565058a380c93a98058fde36956f3859e7
Maureen E <maureenekeugo1@gmail.com>
2024-07-01 12:50:53 -0400

    Created and styled side bar and gave it some functionality

---
+++ script.js
@@ -0,0 +1,7 @@
+let menuIcon = document.querySelector('.menu-icon')
+let sidebar = document.querySelector('.sidebar') 
+
+
+menuIcon.onclick = ()=>{
+    sidebar.classList.toggle('small-sidebar')
+}
